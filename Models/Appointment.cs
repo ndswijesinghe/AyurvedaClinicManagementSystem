dbe788bbb12e51676565336d3af97dab840afd2e
Naveen Wijesinghe <nndswijesinghe2004@gmail.com>
2026-06-20 19:59:19 +0530

    Added Doctor and Appointment models

---
+++ Models/Appointment.cs
@@ -0,0 +1,10 @@
+﻿namespace AyurvedaClinicManagementSystem.Models
+{
+    public class Appointment
+    {
+        public int AppointmentID { get; set; }
+        public int PatientID { get; set; }
+        public int DoctorID { get; set; }
+        public DateTime AppointmentDate { get; set; }
+    }
+}
---
+++ Models/Doctor.cs
@@ -0,0 +1,10 @@
+﻿namespace AyurvedaClinicManagementSystem.Models
+{
+    public class Doctor
+    {
+        public int DoctorID { get; set; }
+        public string Name { get; set; } = "";
+        public string Specialization { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+    }
+}
